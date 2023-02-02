1b04ad08a0c49056e7c3ad76e2c3a8b96e40afe9
jasonyoo3026 <jasonyoo3026@gmail.com>
2023-02-02 21:40:07 +1100

    minor changes

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,29 +1,29 @@
 const Intern = require('../lib/Intern.js');
 const intern = new Intern('jason', '34', 'jasonyoo0326@gmail.com', 'USYD');
 
-test('test if we can get the constructor values for the intern object', () => {
+test('should get the inputs for the intern object', () => {
     expect(intern.name).toBe('jason');
     expect(intern.id).toBe('34');
     expect(intern.email).toBe('jasonyoo0326@gmail.com');
     expect(intern.school).toBe('USYD');
 });
 
-test('test if we can get the name from the getName() method', () => {
+test('should get the name from the getName() method', () => {
     expect(intern.getName()).toBe('jason');
 });
 
-test('test if we can get the id from the getId() method', () => {
+test('should get the id from the getId() method', () => {
     expect(intern.getId()).toBe('34');
 });
 
-test('test if we can get the email from the getEmail() method', () => {
+test('should get the email address from the getEmail() method', () => {
     expect(intern.getEmail()).toBe('jasonyoo0326@gmail.com');
 });
 
-test('test if we can get the school name from the getSchool() method', () => {
+test('should get name of the school from the getSchool() method', () => {
     expect(intern.getSchool()).toBe('USYD');
 });
 
-test('test if we can get the role from the getRole() method', () => {
+test('should get the role from the getRole() method', () => {
     expect(intern.getRole()).toBe('Intern');
 });
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,29 +1,29 @@
 const Manager = require('../lib/Manager.js');
 const manager = new Manager('jason', '34', 'jasonyoo0326@gmail.com', '876');
 
-test('test if we can get the constructor values for the manager object', () => {
+test('should get the inputs for the manager object', () => {
     expect(manager.name).toBe('jason');
     expect(manager.id).toBe('34');
     expect(manager.email).toBe('jasonyoo0326@gmail.com');
     expect(manager.officeNumber).toBe('876');
 });
 
-test('test if we can get the name from the getName() method', () => {
+test('should get the name from the getName() method', () => {
     expect(manager.getName()).toBe('jason');
 });
 
-test('test if we can get the id from the getId() method', () => {
+test('should get the id from the getId() method', () => {
     expect(manager.getId()).toBe('34');
 });
 
-test('test if we can get the email from the getEmail() method', () => {
+test('should get the email address from the getEmail() method', () => {
     expect(manager.getEmail()).toBe('jasonyoo0326@gmail.com');
 });
 
-test('test if we can get the office number from the getOfficeNumber() method', () => {
+test('should get the office number from the getOfficeNumber() method', () => {
     expect(manager.getOfficeNumber()).toBe('876');
 });
 
-test('test if we can get the role from the getRole() method', () => {
+test('should get the role from the getRole() method', () => {
     expect(manager.getRole()).toBe('Manager');
 });
